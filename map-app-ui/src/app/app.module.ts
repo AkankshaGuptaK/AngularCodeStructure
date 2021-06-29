cd836c444953044d0a37caa5f8e49acce8a52cf5
Abdul Aleem <abdul.a.syed@pwc.com>
2021-06-29 09:43:20 -0400

    map app initial ui

---
+++ map-app-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './core/layout/layout.component';
+import { AuthComponent } from './core/auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
